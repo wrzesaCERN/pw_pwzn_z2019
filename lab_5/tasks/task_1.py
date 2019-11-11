b9517f628d843cfa952b09922ea8a1b89fb8e272
Wioleta Rzesa <01110167@pw.edu.pl>
2019-11-11 21:11:14 +0100

    laby5 - chyba wyszło

---
+++ lab_5/tasks/task_1.py
@@ -0,0 +1,132 @@
+"""
+Uzupełnij klasę Calculator o obsługę wyjątków (1pkt).
+Podnoszone wyjątki:
+- błędna operacja - WrongOperation
+- błędne argumenty (nie liczba) - NotNumberArgument
+- pusta pamięć (zarówno przy pytaniu o pamięć
+jak i operację z argumentem domyślnym) - EmptyMemory
+- dzielenie przez zero jest przekształcane w CalculatorError
+"""
+from operator import add, mul, sub, truediv
+
+
+class CalculatorError(Exception):
+    pass
+
+
+class WrongOperation(CalculatorError):
+    pass
+
+
+class NotNumberArgument(CalculatorError):
+    pass
+
+
+class EmptyMemory(CalculatorError):
+    pass
+
+
+class Calculator:
+    operations = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': truediv,
+    }
+
+    def __init__(self):
+        self._memory = None
+        self._short_memory = None
+
+    def run(self, operator, arg1, arg2=None):
+        """
+        Returns result of given operation.
+        :param operator: sign of operation to perform
+        :type operator: str
+        :param arg1: first argument, must be a numeric value
+        :type arg1: float
+        :param arg2: optional, second argument, must be a numeric value
+        :type arg2: float
+        :return: result of operation
+        :rtype: float
+        """
+
+        if operator in self.operations:
+            if arg2 is not None:
+                arg2 = arg2
+            else:
+                arg2 = self.memory
+            if arg2 is not None:
+                try:
+                    self._short_memory = self.operations[operator](float(arg1), float(arg2))
+                except ZeroDivisionError as exc:
+                    raise CalculatorError from exc
+                except ValueError as exc:
+                    raise NotNumberArgument from exc
+                return self._short_memory
+            
+            else:
+                raise EmptyMemory
+            
+        else:
+            raise WrongOperation
+
+    @property
+    def memory(self):
+        return self._memory
+
+    def memorize(self):
+        """Saves last operation result to memory."""
+        self._memory = self._short_memory
+
+    def clean_memory(self):
+        """Cleans memorized value"""
+        self._memory = None
+
+    def in_memory(self):
+        """Prints memorized value."""
+        if self.memory is None:
+            raise EmptyMemory
+        else:
+            print(f"Zapamiętana wartość: {self.memory}")
+
+
+if __name__ == '__main__':
+    b = None
+    calc = Calculator()
+
+    try:
+        b = calc.run('+', 1, 'a')
+    except CalculatorError as exc:
+        assert type(exc) == NotNumberArgument
+        assert b is None
+
+    try:
+        b = calc.run('^', 2, 3)
+    except CalculatorError as exc:
+        assert type(exc) == WrongOperation
+        assert b is None
+
+    try:
+        calc.in_memory()
+    except CalculatorError as exc:
+        assert type(exc) is EmptyMemory
+    else:
+        raise AssertionError
+
+    try:
+        b = calc.run('/', 2)
+    except CalculatorError as exc:
+        assert type(exc) == EmptyMemory
+        assert b is None
+    else:
+        raise AssertionError
+
+    try:
+        b = calc.run('/', 1, 0)
+    except CalculatorError as exc:
+        assert type(exc.__cause__) == ZeroDivisionError
+        assert b is None
+    else:
+        raise AssertionError
+
--- lab_5/tasks/task_2.py
+++ lab_5/tasks/task_2.py
@@ -1,5 +1,3 @@
-
-Learn more or give us feedback
 """
 Na (1 pkt.):
 - Zaimplementuj klasy: Rectangle, Square, Circle dziedziczące z klasy Figure
@@ -28,6 +26,9 @@ klasy Square o takich przekątnych.
 (w klasie Square)
 """
 
+import math
+from abc import ABC
+
 
 class Figure:
     def area(self):
@@ -36,30 +37,149 @@ class Figure:
     def perimeter(self):
         raise NotImplementedError
 
-    def name(self):
-        raise NotImplementedError
+    @classmethod
+    def name(cls): return cls.__name__
 
     def __str__(self):
         return (
-            f'{self.name()}: area={self.area():.3f}, '
-            f'perimeter={self.perimeter():.3f}'
+            f'{self.name()}: area={self.area:.3f}, '
+            f'perimeter={self.perimeter:.3f}'
         )
 
 
-class Circle:
-    pass
+class Circle(Figure):
+
+    def __init__(self, r): self.r = r
+
+    @property
+    def r(self): return self.__r
+
+    @r.setter
+    def r(self, r): self.__r = r
+
+    @staticmethod
+    def get_area(r): return math.pi * r * r
+
+    @property
+    def area(self): return self.get_area(self.r)
+
+    @staticmethod
+    def get_perimeter(r): return math.pi * r * 2
+
+    @property
+    def perimeter(self): return self.get_perimeter(self.r)
 
 
-class Rectangle:
-    pass
+class Rectangle(Figure):
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @property
+    def a(self): return self.__a
+
+    @property
+    def b(self): return self.__b
+
+    @a.setter
+    def a(self, a): self.__a = a
+
+    @b.setter
+    def b(self, b): self.__b = b
+
+    @staticmethod
+    def get_area(a, b): return a * b
+
+    @property
+    def area(self): return self.get_area(self.a, self.b)
+
+    @staticmethod
+    def get_perimeter(a, b): return 2 * a + 2 * b
+
+    @property
+    def perimeter(self): return self.get_perimeter(self.a, self.b)
 
 
-class Square:
-    pass
+class Diamond(Figure):
+
+    def __init__(self, e, f):
+        self.e = e
+        self.f = f
+
+    @property
+    def e(self): return self.__e
+
+    @property
+    def f(self): return self.__f
+
+    @e.setter
+    def e(self, e): self.__e = e
+
+    @f.setter
+    def f(self, f): self.__f = f
+
+    @staticmethod
+    def _area(e, f): return 0.5 * e * f
+
+    @property
+    def area(self): return self._area(self.e, self.f)
+
+    def are_diagonals_equal(self): return self.f == self.e
+
+    def bok(self): return math.sqrt((self.e / 2) ** 2 + (self.f / 2) ** 2)
+
+    @property
+    def perimeter(self): return 4 * self.bok()
+
+    def to_square(self):
+        if self.are_diagonals_equal(): return Square(self.bok())
+        else: raise RuntimeError("Przekątne nie są takiej samej długości")
 
 
-class Diamond:
-    pass
+class Square(Rectangle, Diamond):
+
+    def __init__(self, a): super().__init__(a, a)
+
+    def set_a(self, x):
+        self.__a = x
+        self.__b = x
+        self.__e = math.sqrt(2 * x * x)
+        self.__f = math.sqrt(2 * x * x)
+
+    def get_a(self): return self.__a
+
+    a = property(get_a, set_a)
+
+    def set_b(self, y):
+        self.__a = y
+        self.__b = y
+        self.__e = math.sqrt(2 * y * y)
+        self.__f = math.sqrt(2 * y * y)
+
+    def get_b(self): return self.__b
+
+    b = property(get_b, set_b)
+
+    def set_e(self, e):
+        self.__a = math.sqrt(e * e / 2)
+        self.__b = math.sqrt(e * e / 2)
+        self.__e = e
+        self.__f = e
+
+    def get_e(self): return self.__e
+
+    e = property(get_e, set_e)
+
+    def set_f(self, f):
+        self.__a = math.sqrt(f * f / 2)
+        self.__b = math.sqrt(f * f / 2)
+        self.__e = f
+        self.__f = f
+
+    def get_f(self): return self.__f
+
+    f = property(get_f, set_f)
 
 
 if __name__ == '__main__':
@@ -71,7 +191,7 @@ if __name__ == '__main__':
 
     # print("Square")
     sqr_1 = Square(4)
-    assert str(sqr_1) == 'Square: area=8.000, perimeter=16.000'
+    assert str(sqr_1) == 'Square: area=16.000, perimeter=16.000'
 
     diam_1 = Diamond(6, 8)
     assert str(diam_1) == 'Diamond: area=24.000, perimeter=20.000'
@@ -80,4 +200,4 @@ if __name__ == '__main__':
     assert str(diam_2) == 'Diamond: area=0.500, perimeter=2.828'
 
     sqr_3 = diam_2.to_square()
-    assert str(diam_2) == 'Square: area=0.500, perimeter=2.828'
+    assert str(sqr_3) == 'Square: area=0.500, perimeter=2.828'
