99ee3ea68b8f035582655b31107046d73658d859
Wioleta Rzesa <01110167@pw.edu.pl>
2020-01-20 17:56:23 +0100

    rozwiązania - mam nadzieje że o to chodzi...

--- lab_11/tasks/tests/tests_task1.py
+++ lab_11/tasks/tests/tests_task1.py
@@ -1,10 +1,106 @@
 import pytest
 
-from lab_11.tasks.tools.calculator import (
-    Calculator,
-    CalculatorError,
-    EmptyMemory,
-    NotNumberArgument,
-    WrongOperation,
+from tasks.tools.calculator import (
+Calculator,
+CalculatorError,
+EmptyMemory,
+NotNumberArgument,
+WrongOperation,
 )
-#obecnosc
+
+@pytest.fixture()
+def calc():
+    return Calculator()
+
+
+noproblems_test_tab = [
+    ("+", 8, 8, 16),
+    ("-", 8, 6, 2),
+    ("*", 1, 0, 0),
+    ("/", 9, 3, 3),
+    ("+", 3, 2, 5),
+    ("-", 2, 6, -4),
+    ("*", 8, 8, 64),
+    ("/", 100, 5, 20),
+    ("+", 1, 9, 10),
+    ("-", 2, 0, 2),
+    ("*", 4, 6, 24),
+    ("/", 7, 7, 1),
+]
+
+
+@pytest.mark.parametrize("operator,arg1,arg2,expected", noproblems_test_tab)
+def test_calculator(operator, arg1, arg2, expected, calc):
+    assert calc.run(operator, arg1, arg2) == expected
+
+
+exceptions_test_tab = [
+    ("/", 8, 0, CalculatorError),
+    ("*", 8, None, EmptyMemory),
+    ("+", 2, "napis", NotNumberArgument),
+    (6, "-", 8, WrongOperation)
+]
+
+
+@pytest.mark.parametrize("operator, arg1, arg2, expected", exceptions_test_tab)
+def test_afewexeptions(operator, arg1, arg2, expected, calc):
+    try:
+        calc.run(operator, arg1, arg2)
+    except CalculatorError as exc:
+        assert type(exc) == expected
+    else:
+        raise AssertionError
+
+
+def test_emptyMemory_simpleissiue(calc):
+    try:
+        calc.in_memory()
+    except CalculatorError as exc:
+        assert type(exc) is EmptyMemory
+    else:
+        raise AssertionError
+
+
+def test_emptyMemory_morecomplicatedissiue(calc):
+    try:
+        calc.run("-", 4, 2)
+        calc.memorize()
+        assert calc.memory == 2
+        calc.clean_memory()
+        calc.run("*", 6)
+    except CalculatorError as exc:
+        assert type(exc) is EmptyMemory
+    else:
+        raise AssertionError
+
+
+def test_zeroDivision(calc):
+    try:
+        calc.run("/", 100, 0)
+    except CalculatorError as exc:
+        assert type(exc.__cause__) == ZeroDivisionError
+    else:
+        raise AssertionError
+
+
+def test_operation(calc):
+    try:
+        calc.run("^", 2, 2)
+    except CalculatorError as exc:
+        assert type(exc) == WrongOperation
+    else:
+        raise AssertionError
+
+
+def test_notNumber(calc):
+    try:
+        calc.run("+", 3, "nieliczba")
+    except CalculatorError as exc:
+        assert type(exc) == NotNumberArgument
+    else:
+        raise AssertionError
+
+
+
+
+
--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,5 +1,50 @@
-import pytest
+import requests
+import requests_mock
 
-from lab_11.tasks.tools.metaweather import (
-    get_metaweather
+from tasks.tools.metaweather import (
+    get_metaweather,
+    get_cities_woeid
 )
+
+url = 'https://www.metaweather.com/api/location/search'
+
+tab_cit = [{'title': 'Warsaw', 'location_type': 'City', 'woeid': 523920, 'latt_long': '52.235352,21.009390'},
+          {'title': 'Newark', 'location_type': 'City', 'woeid': 2459269, 'latt_long': '40.731972,-74.174179'}]
+
+
+def test_empty():
+    with requests_mock.Mocker() as m:
+        m.get(url, json=[], status_code=200)
+        assert get_cities_woeid("Warszawa") == {}
+
+
+def test_notempty():
+    with requests_mock.Mocker() as m:
+        m.get(url, json=tab_cit, status_code=200)
+        assert get_cities_woeid("War") == {
+            "Warsaw": 523920,
+            "Newark": 2459269
+        }
+
+
+def test_status():
+    try:
+        with requests_mock.Mocker() as m:
+            m.get(url, status_code=404)
+        get_cities_woeid("War")
+    except Exception as exc:
+        assert isinstance(exc, requests.exceptions.HTTPError)
+
+
+def test_timeout():
+    try:
+        with requests_mock.Mocker() as m:
+            m.get(url, exc=requests.exceptions.Timeout)
+        get_cities_woeid("War", 0.1)
+    except Exception as exc:
+        assert isinstance(exc, requests.exceptions.Timeout)
+
+
+
+
+
